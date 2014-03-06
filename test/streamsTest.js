afef38ddc3765440c5206de734f2885bca8d812b
glukki <glukki.spb.ru@gmail.com>
2014-03-06 14:03:46 +0400

    предотвращение преждевременного закрытия потока

--- test/streamsTest.js
+++ test/streamsTest.js
@@ -2,16 +2,15 @@
 
 var http = require( 'http' );
 var mongodb = require( '../lib/mongodb/index' );
-var through = require( 'through' );
 
-
+function tr( data ) { return JSON.stringify( data ) + '\n'; }
 
 mongodb.MongoClient.connect( 'mongodb://localhost/test', function( err, db ) {
 	http.createServer(function( req, res ) {
-		db.collection( 'stream' ).find().stream()
-			.pipe( through( function( data ) {
-				this.queue( JSON.stringify( data ) + '\n' );
-			} ) )
+		var cursor = db.collection( 'stream' )
+			.find()
+			.batchSize( 100 )
+			.stream( { transform: tr } )
 			.pipe( res );
 //		db.collection( 'stream' ).find().toArray( function( err, data ) {
 //			for ( var i = 0; i < data.length; i += 1 ) {
